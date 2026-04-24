51a41df5e4e3f3d42e7c632740821702ff55038f
Asfana <katheejathasfana@gmail.com>
2026-04-24 17:08:51 +0530

    added gunicorn

--- hotel/urls.py
+++ hotel/urls.py
@@ -1,26 +1,3 @@
-"""hotel URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-from django.contrib import admin
-from django.urls import path, include
-
-import broomk.views as views
-from django.contrib.auth import views as auth_views
-from django.conf import settings
-from django.conf.urls.static import static
-
 urlpatterns = [
     path('', views.homepage, name="home"),
     path('login/', views.user_login, name="login"),
@@ -32,8 +9,9 @@ urlpatterns = [
     path('contact/', views.contactpage, name="contact"),
     path('dashboard/', views.user_dashboard, name="dashboard"), 
     path('admin/', admin.site.urls),
-    path('booking-confirmation/<int:id>/',views.booking_confirmation, name="confirmation"),
-
+    path('booking-confirmation/<int:id>/', views.booking_confirmation, name="confirmation"),
     path('staff/', include('app.urls'))
-    
-]urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
+
+# ✅ THIS MUST BE ON NEW LINE
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
